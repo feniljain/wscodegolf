915b504117038c112caecc69b43258e4143309f5
feniljain <fkjainco@gmail.com>
2023-12-04 11:44:55 +0530

    wip: try to replace with syscalls only
    currently getting conn reset by peer error

---
+++ test/go.mod
@@ -0,0 +1,3 @@
+module wscodegolftest
+
+go 1.20
--- test/main.go
+++ test/main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"net"
-	"os"
+	"fmt"
+	"syscall"
 )
 
 func main() {
-	httpInitMsg := "GET /echo HTTP/1.1\r\nHost: localhost.com:8080\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\nConnection: keep-alive, Upgrade\r\nSec-Fetch-Mode: websocket\r\n\r\n"
-
-	conn, _ := net.Dial("tcp", "localhost:8080")
-
-	data := []byte(httpInitMsg)
-	conn.Write(data)
-
-	buf := make([]byte, 1024)
-	conn.Read(buf)
-
-	data = []byte{
+	packet := []byte{
 		0b10000001, // FIN, RSV1, RSV2, RSV3, OpCode
 		0b10000101, // Mask Bit (Compulsary for client to set) + Payload
 		// NOTE: We don't need to set extended payload bits if our
@@ -31,9 +21,49 @@ func main() {
 		0b01101000,
 		0b01101110, // Payload
 	}
-	conn.Write(data)
+	var response []byte
 
-	n, _ := conn.Read(buf)
+	sockfd, _ := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
 
-	os.Stdout.Write(buf[2:n])
+	serverAddr := &syscall.SockaddrInet4{
+		Port: 8080,
+		Addr: [4]byte{127, 0, 0, 1},
+	}
+
+	syscall.Connect(sockfd, serverAddr)
+
+	fmt.Println("connected!")
+
+	err := syscall.Sendto(sockfd, []byte("GET /echo HTTP/1.1\r\nHost: localhost.com:8080\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n"), 0, nil)
+	// err := syscall.Sendmsg(sockfd, []byte("GET /echo HTTP/1.1\r\nHost: localhost.com:8080\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\nSec-WebSocket-Version: 13\r\n\r\n"), nil, serverAddr, syscall.MSG_DONTWAIT)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("http msg sent!")
+
+	data := []byte{}
+	_, _, err = syscall.Recvfrom(sockfd, response, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("msg read!", data)
+
+	err = syscall.Sendto(sockfd, packet, 0, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("ws read!")
+
+	_, _, err = syscall.Recvfrom(sockfd, response, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("ws resp read!")
+
+	fmt.Println("resp:", response)
+	syscall.Write(1, response)
 }
