c4f3035c9ce42628d38cf0e67e4375966b740ff0
feniljain <fkjainco@gmail.com>
2023-12-19 23:07:12 +0530

    4620 bytes

--- test/main.go
+++ test/main.go
@@ -52,26 +52,26 @@ func alloc(size uintptr, layout unsafe.Pointer) unsafe.Pointer {
 
 func main() {	
 	// __NR_socket, AF_INET, SOCK_STREAM
-	var sock, _, _ = syscall.Syscall(41, 0x2, 0x1, 0)
+	var sock, _, _ = syscall.Syscall(359, 0x2, 0x1, 0)
 
 	// __NR_connect, fd, sockaddr_in, len(sockaddr_in)
-	syscall.Syscall6(42, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
+	syscall.Syscall6(362, sock, uintptr(unsafe.Pointer(&sockaddr[0])), uintptr(len(sockaddr)), 0, 0, 0)
 
 	// __NR_sendto, fd, buf, len(buf), flags, addr, addr_len
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
+	syscall.Syscall6(369, sock, uintptr(unsafe.Pointer(&httpInitMsg[0])), uintptr(len(httpInitMsg)), 0, 0, 0)
 
 	// __NR_recvfrom, fd, buf, len(buf), flags, addr, addr_len
-	var n, _, _ = syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
+	var n, _, _ = syscall.Syscall6(371, sock, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)), 0, 0, 0)
 
 	// __NR_sendto
-	syscall.Syscall6(44, sock, uintptr(unsafe.Pointer(&packet[0])), uintptr(len(packet)), 0, 0, 0)
+	syscall.Syscall6(369, sock, uintptr(unsafe.Pointer(&packet[0])), uintptr(len(packet)), 0, 0, 0)
 
 	// __NR_recvfrom
-	syscall.Syscall6(45, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
+	syscall.Syscall6(371, sock, uintptr(unsafe.Pointer(&response[n])), uintptr(len(response))-n, 0, 0, 0)
 
 	// __NR_close
-	syscall.Syscall(3, sock, 0, 0)
+	syscall.Syscall(6, sock, 0, 0)
 
 	// __NR_write, STDOUT_FILENO
-	syscall.Syscall(1, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
+	syscall.Syscall(4, 1, uintptr(unsafe.Pointer(&response[0])), uintptr(len(response)))
 }
